a53bf09096deccf51b644fc0928965d9b586431c
Shashwat Vaidya <shashwatvaidya.dev@gmail.com>
2026-08-30 15:56:56 +0530

    Prepare frontend for production deployment

--- server/server.js
+++ server/server.js
@@ -8,8 +8,8 @@ const helmet = require('helmet');
 
 const corsOptions = {
   origin: process.env.NODE_ENV === 'production'
-    ? process.env.CLIENT_URL
-    : 'http://localhost:5173',
+  ? process.env.CLIENT_URL
+  : ['http://localhost:5173', 'http://localhost:4173'],
   credentials: true,
 };
 
